5e152b28bcd7418cf120a59d6adc68c4f848eb5c
JeanDavidDaviet <jeandaviddaviet@gmail.com>
2022-12-24 15:23:37 +0100

    remove dark theme

--- components/Footer.js
+++ components/Footer.js
@@ -2,10 +2,10 @@ import Link from "next/link";
 
 export default function Footer() {
     return (
-        <footer className="p-4 bg-white rounded-lg shadow md:flex md:items-center md:justify-between md:p-6 dark:bg-gray-800">
-            <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <Link href="/" className="hover:underline">Techtalks</Link>
+        <footer className="p-4 bg-white rounded-lg shadow md:flex md:items-center md:justify-between md:p-6">
+            <span className="text-sm text-gray-500 sm:text-center">© 2023 <Link href="/" className="hover:underline">Techtalks</Link>
             </span>
-            <ul className="flex flex-wrap items-center mt-3 text-sm text-gray-500 dark:text-gray-400 sm:mt-0">
+            <ul className="flex flex-wrap items-center mt-3 text-sm text-gray-500 sm:mt-0">
                 <li>
                     <Link href="/" className="mr-4 hover:underline md:mr-6 ">Conferences</Link>
                 </li>

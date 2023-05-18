866ae4225a7d54cb7630adb98570cf315306e3ed
Jean-David Daviet <jean-david.daviet@adeliom.com>
2023-05-18 09:48:13 +0200

    update layout of header and footer

--- components/Footer.js
+++ components/Footer.js
@@ -2,9 +2,9 @@ import Link from "next/link";
 
 export default function Footer() {
     return (
-        <footer className="px-2 sm:px-4">
+        <footer className="px-2 sm:px-4 pb-4">
             <div className="p-4 bg-white rounded-lg shadow md:flex md:items-center md:justify-between md:p-6">
-                <span className="text-sm text-gray-500 sm:text-center">© 2023 <Link href="/" className="hover:underline">Techtalks</Link>
+                <span className="text-sm text-gray-500 sm:text-center">© 2023 <Link href="/" className="hover:underline">Tech confs</Link>
                 </span>
                 <ul className="flex flex-wrap items-center mt-3 text-sm text-gray-500 sm:mt-0">
                     <li>
--- components/Header.js
+++ components/Header.js
@@ -12,11 +12,11 @@ export default function Header({ currentPage }) {
 
     return (
         <nav className="bg-white border-gray-200 px-2 sm:px-4 py-2.5 rounded">
-            <div className="container flex flex-wrap justify-between items-center mx-auto">
-                <Link href="/" className="flex items-center">
+            <div className="flex justify-between items-center md:relative">
+                <Link href="/" className="flex items-center md:absolute md:left-0">
                     <span className="self-center text-xl font-semibold whitespace-nowrap">Tech confs</span>
                 </Link>
-                <div className="flex md:order-2">
+                <div className="flex md:order-2 ml-auto">
                     {/* <div className="hidden relative md:block">
                         <div className="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
                             <svg className="w-5 h-5 text-gray-500" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clipRule="evenodd"></path></svg>
@@ -29,7 +29,7 @@ export default function Header({ currentPage }) {
                         <svg className="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clipRule="evenodd"></path></svg>
                     </button>
                 </div>
-                <div className={(menuOpen ? "" : "hidden") + " justify-between items-center w-full md:flex md:w-auto md:order-1"} id="navbar-search">
+                <div className={(menuOpen ? "" : "hidden") + " justify-between items-center w-full md:flex md:order-1 md:justify-center"} id="navbar-search">
                     {/* <div className="relative mt-3 md:hidden">
                         <div className="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
                             <svg className="w-5 h-5 text-gray-500" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clipRule="evenodd"></path></svg>

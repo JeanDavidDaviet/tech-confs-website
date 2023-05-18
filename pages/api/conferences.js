80c3759ad049184b7b409d76aee1f6af63c58f2d
JeanDavidDaviet <jeandaviddaviet@gmail.com>
2023-05-18 10:18:10 +0200

    allow filtering api by query params

--- pages/api/conferences.js
+++ pages/api/conferences.js
@@ -4,5 +4,33 @@ import { promises as fs } from 'fs';
 export default async function handler(req, res) {
   const jsonDirectory = path.join(process.cwd(), 'json');
   const fileContents = await fs.readFile(jsonDirectory + '/data.json', 'utf8');
-  res.status(200).json(JSON.parse(fileContents));
+  const json = JSON.parse(fileContents);
+
+  const filters = Object.keys(req.query);
+
+  const response = json.filter((conf) => {
+    let resolve = true;
+    return filters.map(filter => {
+
+      if(filter in conf && (conf[filter] === null || conf[filter] === undefined)){
+        return false;
+      }
+      
+      if(Array.isArray(conf[filter])){
+        if(! conf[filter].includes(req.query[filter])){
+          resolve = false;
+        }
+      } else {
+        if(conf[filter].toString() !== req.query[filter].toString()){
+          resolve = false;
+        }
+      }
+
+      return resolve;
+      
+    }).every(e => e === true)
+
+  });
+
+  res.status(200).json(response);
 }
